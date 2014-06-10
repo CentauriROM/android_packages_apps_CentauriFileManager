86de16f8fee3aeb9ebc7c98970bfeb393e626452
Tony Malagisi <mtb3000gt@gmail.com>
2014-06-10 17:03:04 -0400

    CentauriROM Bring-Up

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
-LOCAL_PACKAGE_NAME := LiquidFileManager
+LOCAL_PACKAGE_NAME := CentauriFileManager
 LOCAL_CERTIFICATE := platform
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
